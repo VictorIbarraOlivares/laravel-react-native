6d29cb35958cf7952c52bb53f2b43a59f6f039c0
unknown <victor.ibarra.olivares@gmail.com>
2022-02-23 17:57:33 -0300

    pantallas de login y registro sin logica para iniciar la navegacion

--- ProgrammerSopolis/src/screens/auth/Login.js
+++ ProgrammerSopolis/src/screens/auth/Login.js
@@ -1,92 +1,30 @@
 import React, { useState } from "react";
-import { View } from "react-native";
-import styles from "../../styles/auth";
-import { Text, Button, Image } from "react-native-elements";
-import Toast from "react-native-root-toast";
+import { SafeAreaView, Text, View } from "react-native";
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-import { ErrorText, ActivityLoader } from "../../components/Shared";
-
-import { useForm } from "react-hook-form";
-import { EmailInput, PasswordInput } from "../../components/inputs";
-
-import { useAuth } from "../../providers/AuthProvider";
-import { login } from "../../services/AuthService";
-
-const Login = ({ navigation }) => {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [secureEntry, setSecureEntry] = useState(true);
-  const { control, handleSubmit, formState: { errors }} = useForm();
-  const { handleLogin } = useAuth();
-
-  const _login = async (data) => {
-    try {
-      setLoading(true);
-      const response = await login(data);
-      await handleLogin(response.data);
-      Toast.show(
-          response.message,
-        {
-          position: Toast.positions.CENTER,
-        }
-      );
-    } catch (e) {
-      setError(e.message);
-      setLoading(false);
-    }
-  }
-
-  const toggleSecureEntry = () => {
-    setSecureEntry(!secureEntry);
-  }
-
+const Login = () => {
   return (
-      <View style={styles.container}>
-        {loading === true ? <ActivityLoader /> : null}
-
-        <Image
-          style={{width: 100, height: 100, marginBottom: 20}}
-          source={require("../../../assets/app_icon.png")}
-        />
-
-        <Text h2 style={styles.title}>Inicio de sesión</Text>
-
-        <ErrorText error={error} />
-
-        <EmailInput
-          name="email"
-          control={control}
-          errors={errors}
-          errorValidationStyle={styles.errorValidation}
-          inputStyle={styles.input}
-        />
-
-        <PasswordInput
-          name="password"
-          control={control}
-          errors={errors}
-          errorValidationStyle={styles.errorValidation}
-          inputStyle={styles.input}
-          secureEntry={secureEntry}
-          toggleSecureEntry={toggleSecureEntry}
-        />
-
-        <Button
-          titleStyle={styles.buttonTitle}
-          buttonStyle={styles.button}
-          title="Acceder"
-          type="outline"
-          onPress={handleSubmit(_login)}
-        />
-
-        <Text
-          onPress={() => navigation.navigate("Signup")}
-          style={styles.link}
-        >
-          ¿No tienes una cuenta?
-        </Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text styles={styles.text}>Login</Text>
       </View>
+    </SafeAreaView>
   );
 }
 
 export default Login;
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '$authBg',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '$primary',
+    fontFamily: '$400Regular',
+    fontWeight: '$fontWeight600',
+    fontSize: '$font24',
+  }
+});
--- ProgrammerSopolis/src/screens/auth/Signup.js
+++ ProgrammerSopolis/src/screens/auth/Signup.js
@@ -1,111 +1,30 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import { Text, Button, Image } from 'react-native-elements';
-import Toast from "react-native-root-toast";
-import styles from "../../styles/auth";
-import { ErrorText, ActivityLoader } from "../../components/Shared";
-import { useForm } from "react-hook-form";
-import { EmailInput, PasswordInput, TextInput } from "../../components/inputs";
-import { signup } from "../../services/AuthService";
+import React, { useState } from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-const Signup = ({ navigation }) => {
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [secureEntry, setSecureEntry] = useState(true);
-    const [secureConfirmationEntry, setSecureConfirmationEntry] = useState(true);
-    const { control, handleSubmit, formState: { errors } } = useForm();
-
-    const _signup = async (data) => {
-        try {
-            setLoading(true);
-            const message = await signup(data);
-            await navigation.navigate("Login");
-            Toast.show(
-                message,
-                {}
-            );
-        } catch (e) {
-            setError(e.message);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    const toggleSecureEntry = () => {
-        setSecureEntry(!secureEntry)
-    }
-
-    const toggleSecureConfirmationEntry = () => {
-        setSecureConfirmationEntry(!secureConfirmationEntry)
-    }
-
-    return (
-        <View style={styles.container}>
-            {loading === true ? <ActivityLoader /> : null}
-
-            <Image
-                style={{ width: 100, height: 100, marginBottom: 20, }}
-                source={require("../../../assets/app_icon.png")}
-            />
-
-            <Text h2 style={styles.title}>Registro</Text>
-
-            <ErrorText error={error} />
-
-            <TextInput
-                name="name"
-                minLength={2}
-                maxLength={30}
-                iconName="person-outline"
-                control={control}
-                errors={errors}
-                errorValidationStyle={styles.errorValidation}
-                inputStyle={styles.input}
-            />
-
-            <EmailInput
-                name="email"
-                control={control}
-                errors={errors}
-                errorValidationStyle={styles.errorValidation}
-                inputStyle={styles.input}
-            />
-
-            <PasswordInput
-                name="password"
-                control={control}
-                errors={errors}
-                errorValidationStyle={styles.errorValidation}
-                inputStyle={styles.input}
-                secureEntry={secureEntry}
-                toggleSecureEntry={toggleSecureEntry}
-            />
-
-            <PasswordInput
-                name="passwordConfirmation"
-                control={control}
-                errors={errors}
-                errorValidationStyle={styles.errorValidation}
-                inputStyle={styles.input}
-                secureEntry={secureConfirmationEntry}
-                toggleSecureEntry={toggleSecureConfirmationEntry}
-            />
-
-            <Button
-                titleStyle={styles.buttonTitle}
-                buttonStyle={styles.button}
-                title="Crear cuenta"
-                type="outline"
-                onPress={handleSubmit(_signup)}
-            />
-
-            <Text
-                onPress={() => navigation.navigate("Login")}
-                style={styles.link}
-            >¿Ya tienes una cuenta?</Text>
-
-        </View>
-    );
+const Signup = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text styles={styles.text}>Signup</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 export default Signup;
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '$authBg',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '$primary',
+    fontFamily: '$400Regular',
+    fontWeight: '$fontWeight600',
+    fontSize: '$font24',
+  }
+});
