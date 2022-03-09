2ac909eaa9d7e873e74b9d9cfbe23b8ec10c08ac
unknown <victor.ibarra.olivares@gmail.com>
2022-03-09 19:12:18 -0300

    se finaliza el AuthProvider, login, logout, hooks y context

--- ProgrammerSopolis/src/providers/AuthProvider.js
+++ ProgrammerSopolis/src/providers/AuthProvider.js
@@ -11,4 +11,55 @@ export const AuthContext = createContext();
 
 const AuthProvider = () => {
     const [state, dispatch] = useReducer(AuthReducer, initialState);
+    useEffect(() => {
+        const bootstrapAsync = async () => {
+            let userToken;
+
+            try {
+                userToken = await getItemAsync(USER_TOKEN_KEY);
+            } catch (e) {
+                alert("El token no se ha podido restaurar, cierra la APP");
+            }
+
+            dispatch({ type: RESTORE_TOKEN, token: userToken });
+        }
+
+        bootstrapAsync().then(() => {});
+    });
+
+    const handleLogin = async ({ token, user }) => {
+        try {
+            dispatch({ type: SIGN_IN, token, user});
+        } catch (error) {
+            // TODO crear un toast para informar al usuario que algo esta mal
+            throw new Error(error);
+        }
+    }
+
+    const handleLogout = async () => {
+        try {
+            delete axios.default.headers.common["Authorization"];
+        } catch (error) {
+            // TODO crear un toast para informar al usuario que algo esta mal
+            throw new Error(error);
+        } finally {
+            dispatch({ type: SIGN_OUT});
+        }
+    }
+
+    const authContext = useMemo( () => {
+        return {
+            state, handleLogin, handleLogout
+        }
+    }, [state]);
+
+    return (
+        <AuthContext.Provider value={authContext}>
+            <AppNavigation userToken={state.userToken} />
+        </AuthContext.Provider>
+    );
 }
+
+const useAuth = () => useContext(AuthContext);
+
+export { useAuth, AuthProvider };
