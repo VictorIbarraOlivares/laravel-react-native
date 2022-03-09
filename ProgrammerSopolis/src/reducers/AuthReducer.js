a83e2ebd564cedef4f8ff6643ce152f336de6b3c
unknown <victor.ibarra.olivares@gmail.com>
2022-03-09 18:47:20 -0300

    se crea AuthReducer

---
+++ ProgrammerSopolis/src/reducers/AuthReducer.js
@@ -0,0 +1,36 @@
+export const RESTORE_TOKEN = "RESTORE_TOKEN";
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
+
+export const initialState = {
+    userToken: null,
+    user: null
+}
+
+const AuthReducer = (prevState = initialState, action) => {
+    switch (action.type) {
+        case RESTORE_TOKEN: {
+            return {
+                ...prevState,
+                userToken: action.token,
+                user: action.user
+            }
+        }
+        case SIGN_IN: {
+            return {
+                ...prevState,
+                userToken: action.token,
+                user: action.user
+            }
+        }
+        case SIGN_OUT: {
+            return {
+                ...prevState,
+                userToken: null,
+                user: null
+            }
+        }
+    }
+}
+
+export default AuthReducer;
