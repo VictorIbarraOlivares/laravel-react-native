abb5e5db368c08a6cba336512bd8cf42bf42d2cd
unknown <victor.ibarra.olivares@gmail.com>
2022-03-09 19:24:59 -0300

    se ajusta configuracion Axios para enviar token en cada peticion

--- ProgrammerSopolis/src/utils/axios.js
+++ ProgrammerSopolis/src/utils/axios.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 import { Constants } from "expo-constants";
+import { USER_TOKEN_KEY } from "../providers/AuthProvider";
 
 const axiosInstance = axios.create({
     baseURL: Constants.manifest.extra.api_url + "/",
@@ -11,5 +12,11 @@ const axiosInstance = axios.create({
         accept: 'application/json',
     }
 });
+
+axiosInstance.interceptors.request.use(async req => {
+    const access_token = await SecureStore.getItemAsync(USER_TOKEN_KEY);
+    req.headers.authorization = `Bearer ${access_token}`;
+    return req;
+})
 
 export default axiosInstance;
